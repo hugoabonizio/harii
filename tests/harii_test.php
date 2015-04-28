230ed3827593268d0e4cf575cd7f5a7ad4983638
hugoabonizio <hugo_abonizio@hotmail.com>
2015-04-28 20:13:46 +0000

    Improve test case

--- tests/harii_test.php
+++ tests/harii_test.php
@@ -7,8 +7,13 @@ class HariiTest extends PHPUnit_Framework_TestCase {
 	function testFirstOfAll() {
 		Harii::configure(new PDO('sqlite:' . realpath(dirname(__FILE__)) . '/db/test.db'));
 		$users = User::all();
+		// 0
 		$this->assertEquals("hugo", $users[0]->name);
 		$this->assertEquals("hugo_abonizio@hotmail.com", $users[0]->email);
 		$this->assertEquals("12345", $users[0]->password);
+		// 1
+		$this->assertEquals("karl marx", $users[1]->name);
+		$this->assertEquals("das@kapital.com", $users[1]->email);
+		$this->assertEquals("54321", $users[1]->password);
 	}
 }
