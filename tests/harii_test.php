fca4df589f29b2c904dcb0e2071cb819cbbc7baf
hugoabonizio <hugo_abonizio@hotmail.com>
2015-04-28 20:24:10 +0000

    Test class of result objects and improve tests

--- tests/harii_test.php
+++ tests/harii_test.php
@@ -4,24 +4,29 @@ class User extends Harii {
 }
 
 class HariiTest extends PHPUnit_Framework_TestCase {
+	protected $users;
+	
 	function setUp() {
 		Harii::configure(new PDO('sqlite:' . realpath(dirname(__FILE__)) . '/db/test.db'));
+		$this->users = User::all();
 	}
 	
 	function testFirstOfAll() {
-		$users = User::all();
 		// 0
-		$this->assertEquals("hugo", $users[0]->name);
-		$this->assertEquals("hugo_abonizio@hotmail.com", $users[0]->email);
-		$this->assertEquals("12345", $users[0]->password);
+		$this->assertEquals("hugo", $this->users[0]->name);
+		$this->assertEquals("hugo_abonizio@hotmail.com", $this->users[0]->email);
+		$this->assertEquals("12345", $this->users[0]->password);
 		// 1
-		$this->assertEquals("karl marx", $users[1]->name);
-		$this->assertEquals("das@kapital.com", $users[1]->email);
-		$this->assertEquals("54321", $users[1]->password);
+		$this->assertEquals("karl marx", $this->users[1]->name);
+		$this->assertEquals("das@kapital.com", $this->users[1]->email);
+		$this->assertEquals("54321", $this->users[1]->password);
 	}
 	
 	function testSizeOfResult() {
-		$users = User::all();
-		$this->assertEquals(2, count($users));
+		$this->assertEquals(2, count($this->users));
+	}
+	
+	function testClassOfResults() {
+		$this->assertEquals('User', get_class($this->users[0]));
 	}
 }
