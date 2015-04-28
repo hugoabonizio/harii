63797f0c56d4a205223a584a3b8b1718b3b7aa3d
hugoabonizio <hugo_abonizio@hotmail.com>
2015-04-28 20:05:50 +0000

    Return all records feature implemented!

--- src/harii.php
+++ src/harii.php
@@ -3,13 +3,39 @@ include_once 'helation.php';
 
 class Harii {
 	private static $_PDO;
+	private $_CURRENT_QUERY;
 	
 	static function configure($configs) {
 		self::$_PDO = $configs;
 	}
 	
 	static function all() {
-		$relation = new Helation(array(1, 3, 5, 7, 13));
+		// store the query globaly, this way you can analyse this
+		// later and/or make tests
+		$_CURRENT_QUERY = "SELECT * FROM " . self::make_name() . ";";
+		$result = self::$_PDO->query($_CURRENT_QUERY)->fetchAll(PDO::FETCH_ASSOC);
+		$members = array();
+		$class_name = self::make_name(); // get the class name and strtolower()
+		
+		foreach ($result as $row) {
+			// create a object of the class
+			$m = new $class_name();
+			// and set the attributs
+			foreach ($row as $attr=>$value) {
+				$m->$attr = $value;
+			}
+			$members[] = $m;
+		}
+		
+		$relation = new Helation($members);
 		return $relation;
+	}
+	
+	static function make_name() {
+		if (isset($this)) { // if object context
+			return strtolower(get_class($this));
+		} else { // if calling statically
+			return get_called_class();
+		}
 	}
 }
--- tests/harii_test.php
+++ tests/harii_test.php
@@ -1,5 +1,14 @@
 <?php
+// example class
+class User extends Harii {	
+}
+
 class HariiTest extends PHPUnit_Framework_TestCase {
-	function testTrue() {
+	function testFirstOfAll() {
+		Harii::configure(new PDO('sqlite:' . realpath(dirname(__FILE__)) . '/db/test.db'));
+		$users = User::all();
+		$this->assertEquals("hugo", $users[0]->name);
+		$this->assertEquals("hugo_abonizio@hotmail.com", $users[0]->email);
+		$this->assertEquals("12345", $users[0]->password);
 	}
 }
