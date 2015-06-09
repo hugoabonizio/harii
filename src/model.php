3235d577054b1ec71c82dc301f3dea460bbc4b79
hugoabonizio <hugo_abonizio@hotmail.com>
2015-06-09 15:16:41 +0000

    Model attribute mass assign

--- src/model.php
+++ src/model.php
@@ -2,7 +2,15 @@
 namespace Harii;
 
 class Model extends Harii {
+	function __construct() {
+		if (func_num_args()) {
+			update_attributes(func_get_args()[1]);
+		}
+	}
+	
 	function update_attributes($attrs) {
-		
+		foreach ($attrs as $attr=>$value) {
+			$this->$attrs = $value;
+		}
 	}
 }
